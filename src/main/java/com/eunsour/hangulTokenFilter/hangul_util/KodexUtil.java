e28b97829af612b1f4183e81438fac873b745711
eunsour <woo569628@naver.com>
2022-02-07 22:36:49 +0900

    update Kodex

--- src/main/java/com/eunsour/hangulTokenFilter/hangul_util/KodexUtil.java
+++ src/main/java/com/eunsour/hangulTokenFilter/hangul_util/KodexUtil.java
@@ -251,7 +251,7 @@ public class KodexUtil {
     // 한 글자 분해
     private String decompose(char hangul) {
         if (hangul < '가' || hangul > '힣')
-            return String.valueOf(hangul);
+            return String.valueOf(' ');
 
         StringBuilder jasoBuilder = new StringBuilder();
 
